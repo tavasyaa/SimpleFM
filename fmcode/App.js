5a13e060f4fc2323cbddb9133148aff5d9c26d7f
Tavasya Agarwal <TavasyaAgarwal2020@u.northwestern.edu>
2020-05-13 18:07:20 -0500

    login page basic done

---
+++ fmcode/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Login from './components/Login.js'
+
+export default function App() {
+  return (
+    <Login />
+  );
+}
---
+++ fmcode/components/Login.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { StyleSheet, View, Image, Text, Dimensions } from 'react-native';
+import { Button, Input, Item } from 'native-base';
+import logo from '../assets/nike.png';
+
+export default function Login() {
+  return (
+    <View style={styles.container}>
+    	<Image source={logo} style={styles.loginimage} reSizeMode="contain"/>
+    	<Item regular style={styles.inputs}>
+    		<Input placeholder="Username" />
+    	</Item>
+    	<Item regular style={styles.inputs}>
+    		<Input secureTextEntry={true} placeholder="Password" />
+    	</Item>
+    	<Button style={styles.button}>
+    		<Text style={styles.buttontext}> Login </Text>
+    	</Button>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttontext: {
+  	color: '#fff'
+  },
+  button: {
+  	width: 60,
+  	justifyContent: 'center',
+  },
+  inputs: {
+  	marginTop: 3,
+  	marginBottom: 3,
+  	width: 250
+  }
+})
